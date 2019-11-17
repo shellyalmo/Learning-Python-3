351a574d126be06f2ade36603b0aed86fbd1913b
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2019-11-17 12:06:18 +0200

    project_practicing_string_methods_Thread_Shed.py
    This project is implementing string methods.

---
+++ project_practicing_string_methods_Thread_Shed.py
@@ -0,0 +1,217 @@
+"""This project is implementing string methods. The subject is tallying the number of sales of a sewing shop, calculating the total amount of money made, and keeping track of the names of the customers.
+
+In the begginning, the shop had an out-dating register system and stored all of the transaction information in one huge string called daily_sales.
+
+For each transaction, the name of the customer, amount spent, types of thread purchased, and the date of sale is all recorded in this same string. This program is iterating through this string and cleans up each transaction and stores all the information in easier to access lists."""
+
+#This string is storing all the data and will be split up into individual pieces of data.
+daily_sales = \
+"""Edith Mcbride   ;,;$1.21   ;,;   white ;,; 
+09/15/17   ,Herbert Tran   ;,;   $7.29;,; 
+white&blue;,;   09/15/17 ,Paul Clarke ;,;$12.52 
+;,;   white&blue ;,; 09/15/17 ,Lucille Caldwell   
+;,;   $5.13   ;,; white   ;,; 09/15/17,
+Eduardo George   ;,;$20.39;,; white&yellow 
+;,;09/15/17   ,   Danny Mclaughlin;,;$30.82;,;   
+purple ;,;09/15/17 ,Stacy Vargas;,; $1.85   ;,; 
+purple&yellow ;,;09/15/17,   Shaun Brock;,; 
+$17.98;,;purple&yellow ;,; 09/15/17 , 
+Erick Harper ;,;$17.41;,; blue ;,; 09/15/17, 
+Michelle Howell ;,;$28.59;,; blue;,;   09/15/17   , 
+Carroll Boyd;,; $14.51;,;   purple&blue   ;,;   
+09/15/17   , Teresa Carter   ;,; $19.64 ;,; 
+white;,;09/15/17   ,   Jacob Kennedy ;,; $11.40   
+;,; white&red   ;,; 09/15/17, Craig Chambers;,; 
+$8.79 ;,; white&blue&red   ;,;09/15/17   , Peggy Bell;,; $8.65 ;,;blue   ;,; 09/15/17,   Kenneth Cunningham ;,;   $10.53;,;   green&blue   ;,; 
+09/15/17   ,   Marvin Morgan;,;   $16.49;,; 
+green&blue&red   ;,;   09/15/17 ,Marjorie Russell 
+;,; $6.55 ;,;   green&blue&red;,;   09/15/17 ,
+Israel Cummings;,;   $11.86   ;,;black;,;  
+09/15/17,   June Doyle   ;,;   $22.29 ;,;  
+black&yellow ;,;09/15/17 , Jaime Buchanan   ;,;   
+$8.35;,;   white&black&yellow   ;,;   09/15/17,   
+Rhonda Farmer;,;$2.91 ;,;   white&black&yellow   
+;,;09/15/17, Darren Mckenzie ;,;$22.94;,;green 
+;,;09/15/17,Rufus Malone;,;$4.70   ;,; green&yellow 
+;,; 09/15/17   ,Hubert Miles;,;   $3.59   
+;,;green&yellow&blue;,;   09/15/17   , Joseph Bridges  ;,;$5.66   ;,; green&yellow&purple&blue 
+;,;   09/15/17 , Sergio Murphy   ;,;$17.51   ;,;   
+black   ;,;   09/15/17 , Audrey Ferguson ;,; 
+$5.54;,;black&blue   ;,;09/15/17 ,Edna Williams ;,; 
+$17.13;,; black&blue;,;   09/15/17,   Randy Fleming;,;   $21.13 ;,;black ;,;09/15/17 ,Elisa Hart;,; $0.35   ;,; black&purple;,;   09/15/17   ,
+Ernesto Hunt ;,; $13.91   ;,;   black&purple ;,;   
+09/15/17,   Shannon Chavez   ;,;$19.26   ;,; 
+yellow;,; 09/15/17   , Sammy Cain;,; $5.45;,;   
+yellow&red ;,;09/15/17 ,   Steven Reeves ;,;$5.50   
+;,;   yellow;,;   09/15/17, Ruben Jones   ;,; 
+$14.56 ;,;   yellow&blue;,;09/15/17 , Essie Hansen;,;   $7.33   ;,;   yellow&blue&red
+;,; 09/15/17   ,   Rene Hardy   ;,; $20.22   ;,; 
+black ;,;   09/15/17 ,   Lucy Snyder   ;,; $8.67   
+;,;black&red  ;,; 09/15/17 ,Dallas Obrien ;,;   
+$8.31;,;   black&red ;,;   09/15/17,   Stacey Payne 
+;,;   $15.70   ;,;   white&black&red ;,;09/15/17   
+,   Tanya Cox   ;,;   $6.74   ;,;yellow   ;,; 
+09/15/17 , Melody Moran ;,;   $30.84   
+;,;yellow&black;,;   09/15/17 , Louise Becker   ;,; 
+$12.31 ;,; green&yellow&black;,;   09/15/17 ,
+Ryan Webster;,;$2.94 ;,; yellow ;,; 09/15/17 
+,Justin Blake ;,; $22.46   ;,;white&yellow ;,;   
+09/15/17,   Beverly Baldwin ;,;   $6.60;,;   
+white&yellow&black ;,;09/15/17   ,   Dale Brady   
+;,;   $6.27 ;,; yellow   ;,;09/15/17 ,Guadalupe Potter ;,;$21.12   ;,; yellow;,; 09/15/17   , 
+Desiree Butler ;,;$2.10   ;,;white;,; 09/15/17  
+,Sonja Barnett ;,; $14.22 ;,;white&black;,;   
+09/15/17, Angelica Garza;,;$11.60;,;white&black   
+;,;   09/15/17   ,   Jamie Welch   ;,; $25.27   ;,; 
+white&black&red ;,;09/15/17   ,   Rex Hudson   
+;,;$8.26;,;   purple;,; 09/15/17 ,   Nadine Gibbs 
+;,;   $30.80 ;,;   purple&yellow   ;,; 09/15/17   , 
+Hannah Pratt;,;   $22.61   ;,;   purple&yellow   
+;,;09/15/17,Gayle Richards;,;$22.19 ;,; 
+green&purple&yellow ;,;09/15/17   ,Stanley Holland 
+;,; $7.47   ;,; red ;,; 09/15/17 , Anna Dean;,;$5.49 ;,; yellow&red ;,;   09/15/17   ,
+Terrance Saunders ;,;   $23.70  ;,;green&yellow&red 
+;,; 09/15/17 ,   Brandi Zimmerman ;,; $26.66 ;,; 
+red   ;,;09/15/17 ,Guadalupe Freeman ;,; $25.95;,; 
+green&red ;,;   09/15/17   ,Irving Patterson 
+;,;$19.55 ;,; green&white&red ;,;   09/15/17 ,Karl Ross;,;   $15.68;,;   white ;,;   09/15/17 , Brandy Cortez ;,;$23.57;,;   white&red   ;,;09/15/17, 
+Mamie Riley   ;,;$29.32;,; purple;,;09/15/17 ,Mike Thornton   ;,; $26.44 ;,;   purple   ;,; 09/15/17, 
+Jamie Vaughn   ;,; $17.24;,;green ;,; 09/15/17   , 
+Noah Day ;,;   $8.49   ;,;green   ;,;09/15/17   
+,Josephine Keller ;,;$13.10 ;,;green;,;   09/15/17 ,   Tracey Wolfe;,;$20.39 ;,; red   ;,; 09/15/17 ,
+Ignacio Parks;,;$14.70   ;,; white&red ;,;09/15/17 
+, Beatrice Newman ;,;$22.45   ;,;white&purple&red 
+;,;   09/15/17, Andre Norris   ;,;   $28.46   ;,;   
+red;,;   09/15/17 ,   Albert Lewis ;,; $23.89;,;   
+black&red;,; 09/15/17,   Javier Bailey   ;,;   
+$24.49   ;,; black&red ;,; 09/15/17   , Everett Lyons ;,;$1.81;,;   black&red ;,; 09/15/17 ,   
+Abraham Maxwell;,; $6.81   ;,;green;,;   09/15/17   
+,   Traci Craig ;,;$0.65;,; green&yellow;,; 
+09/15/17 , Jeffrey Jenkins   ;,;$26.45;,; 
+green&yellow&blue   ;,;   09/15/17,   Merle Wilson 
+;,;   $7.69 ;,; purple;,; 09/15/17,Janis Franklin   
+;,;$8.74   ;,; purple&black   ;,;09/15/17 ,  
+Leonard Guerrero ;,;   $1.86   ;,;yellow  
+;,;09/15/17,Lana Sanchez;,;$14.75   ;,; yellow;,;   
+09/15/17   ,Donna Ball ;,; $28.10  ;,; 
+yellow&blue;,;   09/15/17   , Terrell Barber   ;,; 
+$9.91   ;,; green ;,;09/15/17   ,Jody Flores;,; 
+$16.34 ;,; green ;,;   09/15/17,   Daryl Herrera 
+;,;$27.57;,; white;,;   09/15/17   , Miguel Mcguire;,;$5.25;,; white&blue   ;,;   09/15/17 ,   
+Rogelio Gonzalez;,; $9.51;,;   white&black&blue   
+;,;   09/15/17   ,   Lora Hammond ;,;$20.56 ;,; 
+green;,;   09/15/17,Owen Ward;,; $21.64   ;,;   
+green&yellow;,;09/15/17,Malcolm Morales ;,;   
+$24.99   ;,;   green&yellow&black;,; 09/15/17 ,   
+Eric Mcdaniel ;,;$29.70;,; green ;,; 09/15/17 
+,Madeline Estrada;,;   $15.52;,;green;,;   09/15/17 
+, Leticia Manning;,;$15.70 ;,; green&purple;,; 
+09/15/17 ,   Mario Wallace ;,; $12.36 ;,;green ;,; 
+09/15/17,Lewis Glover;,;   $13.66   ;,;   
+green&white;,;09/15/17,   Gail Phelps   ;,;$30.52   
+;,; green&white&blue   ;,; 09/15/17 , Myrtle Morris 
+;,;   $22.66   ;,; green&white&blue;,;09/15/17"""
+
+#------------------------------------------------
+
+"""In the following part of the program we are breaking up daily_sales in easy to understand lists."""
+
+# In order to split up daily_sales by commas, we need to replace the artifact ;,; to something without a comma.
+daily_sales_replaced = daily_sales.replace(';,;','_')
+#test:
+#print(daily_sales_replaced)
+
+#Splitting the string around commas, into a list of each individual transaction (a list of strings). 
+daily_transactions =daily_sales_replaced.split(',')
+#test:
+#print(daily_transactions)
+
+#Splitting each individual transaction into a list of its data points.
+daily_transactions_split = []
+for item in daily_transactions:
+  daily_transactions_split.append(item.split('_'))
+#test:
+#print(daily_transactions_split)
+
+#Iterating through the different data points and stripping off any whitespace. Also adding each cleaned up transaction to a list of lists of strings (transactions_clean).
+transactions_clean = []
+for daily_transactions in daily_transactions_split:
+  transaction_clean = []
+  for transaction in daily_transactions:
+    transaction_clean.append(transaction.strip('\n').strip())
+  transactions_clean.append(transaction_clean)
+    
+#test:
+#print (transactions_clean)
+
+#Creating new lists of strings that will store the individual data points.
+customers = []
+sales = []
+thread_sold = []
+
+#For each transaction, appending the data into the suitable list.
+for data in transactions_clean:
+  customers.append(data[0])
+  sales.append(data[1])
+  thread_sold.append(data[2])
+
+#tests:
+#print(customers)
+#print(sales)
+#print(thread_sold)
+
+"""In the following part of the program we are determining the total value of the days' sales."""
+
+#calculating how much money the shop made in a day.
+total_sales = 0
+#In this loop we are summing up the sales, and removing the $ character, so that we sum floats.
+for sale in sales:
+  value_of_sale = float(sale.strip('$'))
+  total_sales += value_of_sale
+
+#test:
+#print(total_sales)
+
+"""In the following part of the program, we are determining how much thread of any specific color was sold."""
+
+#This list will contain an item for each color thread sold, without multiple color.
+thread_sold_split = []
+#This loop is iterating through each color, and checks if it's a single color or multiple.
+for thread in thread_sold:
+  #If it's multiple (the sign '&' appears), splitting the string around the '&' and appending each individual color to thread_sold_split.
+  # Could use .find() but this is better according to the Python documentation.
+  if '&' in thread:
+    splitted  = thread.split('&')
+    for item in splitted:
+      thread_sold_split.append(item)
+     
+  #If it's a single color, appending it to thread_sold_split.
+  else:
+    thread_sold_split.append(thread)
+  
+#test:
+#print(thread_sold_split)
+
+#This function iterates through thread_sold_split and counts the number of times the item is equal to the color.
+def color_count(color):
+  count = 0
+  for item in thread_sold_split:
+    if item == color:
+      count += 1
+  return count
+
+#test:
+#print(color_count('white')) # should return 28 for 'white'
+
+#This list stores all the colors that the shop offers.
+colors = ['red','yellow','green','white','black','blue','purple']
+
+#Finding out how many threads of wach color were sold in a day.
+for color in colors:
+  for item in thread_sold_split:
+    if color == item:
+      final_string = "Thread Shed sold {} threads of {} thread today.".format(color_count(color),color)
+
+#test:
+#print(final_string)
+  
